cf89fc0853f7f77c395968d10bca75c632594b7b
augusto-dip <augustodip26@gmail.com>
2025-06-17 13:04:22 -0300

    empeando pt1

---
+++ Tareas/Tareas.cs
@@ -0,0 +1,10 @@
+using System.Dynamic;
+using System.Text.Json.Serialization;
+
+public class Tareas
+{
+    [JsonPropertyName("userId")]
+    public int UserId {get;set;}
+    [JsonPropertyName("userId")]
+    public int UserId {get;set;}
+}
